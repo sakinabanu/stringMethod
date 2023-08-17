5c037c9807ecbc86a7ace131d0a6d89ffac89371
sakinabanu <banusakina04@gmail.com>
2023-08-17 12:01:22 +0530

    String Method

---
+++ stringMethodsIntroduction/IntrostringMethod.js
@@ -0,0 +1,7 @@
+/*
+JAVASCRIPT SCRIPT METHODS :
+In JavaScript, strings are used to represent and work with a sequence of characters. 
+A string can represent an object as well as the primitive data type. 
+Each method has a unique function that performs a change or calculation to our string and saves us from writing complex maniplulations from scratch.
+They are 27 methods.
+*/
