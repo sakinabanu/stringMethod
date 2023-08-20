3399b69d20af6077ce8f23ef6ba10f04ea920f8d
sakinabanu <banusakina04@gmail.com>
2023-08-20 23:53:18 +0530

    String Method

--- 25.toLowerCase.js
+++
@@ -1,2 +0,0 @@
-const str="HANZALA IS BAD BOY.";
-console.log(str.toLowerCase());//hanzala is bad boy.
---
+++ stringMethodPractice/25.toLowerCase.js
@@ -0,0 +1,15 @@
+/*
+STRING TOLOWERCASEMETHOD :
+The toLowerCase() method returns the string converted to lowercase.
+        str.toLowerCase()
+The toLowerCase() method does not take in any parameters.
+Returns a new string representing the calling string converted to lowercase.
+Notes:
+The toLowerCase() method raises TypeError when called on null or undefined.
+The toLowerCase() method does not change the original string.
+*/ 
+
+
+
+const a = "MY FAVOURITE LANGUAGE IS JAVASCRIPT."
+console.log(a.toLowerCase()); // my favourite language is javascript.
